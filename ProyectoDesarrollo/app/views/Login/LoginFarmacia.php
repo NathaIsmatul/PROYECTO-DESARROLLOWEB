c11fc165a5160b62dd6567f517ca6fef5cb13677
NathaIsmatul <esdrasismatul@hotmail.com>
2024-10-12 08:40:47 -0600

    Actualizacion
    Vista de Login

---
+++ ProyectoDesarrollo/app/views/Login/LoginFarmacia.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="../../../public/css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="wrapper">
+    <div class="inner">
+        <form method="POST" action="">  <!-- Agregado para levantar el FRONTEND -->
+            <h3>Inicio de</h3>
+            <h3>Sesión</h3>
+            <div class="form-wrapper">
+                <input type="text" name="nombre" id="nombreInput" placeholder="Usuario" class="form-control">
+                <i class="zmdi zmdi-account"></i>
+            </div>
+            <div class="form-wrapper">
+                <input type="password" name="contrasena" placeholder="Contraseña" class="form-control">
+                <i class="zmdi zmdi-lock"></i>
+            </div>
+			<button type="submit">Ingresar <i class="zmdi zmdi-arrow-right"></i></button>
+            <div class="user-type" id="userType">
+            </div>
+        </form>
+    </div>
+</div>
+</body>
+</html>
