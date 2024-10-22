a4c0e129752edf13348f1736450f093000929d77
NathaIsmatul <esdrasismatul@hotmail.com>
2024-10-22 03:39:14 -0600

    Actualizacion

--- ProyectoDesarrollo/app/views/parte_superior.php
+++ ProyectoDesarrollo/app/views/parte_superior.php
@@ -13,6 +13,8 @@
 
     <!-- Custom fonts for this template-->
     <link href="../static/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="../static/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -54,7 +56,7 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Interface
+                Gestion de Usuarios
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
@@ -62,13 +64,12 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="bx bxs-user"></i>
-                    <span>USUARIO</span>
+                    <span>Usuarios</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Datos de Usuario:</h6>
-                        <a class="collapse-item" href="buttons.php">Buttons</a>
-                        <a class="collapse-item" href="cards.php">Cards</a>
+                        <a class="collapse-item" href="usuarios.php">Gestion de Usuarios</a>
+                        <a class="collapse-item" href="perfiles.php">Gestion de Perfiles</a>
                     </div>
                 </div>
             </li>
@@ -122,9 +123,17 @@
             </div>
 
             <li class="nav-item">
-                <a class="nav-link" href="ventas.php">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseData"
+                    aria-expanded="true" aria-controls="collapseData">
                     <i class="bx bx-notepad "></i>
-                    <span>Realizar Vntas</span></a>
+                    <span>Realizar Ventas</span></a>
+                </a>
+                <div id="collapseData" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="ventas.php">Realizar Ventas</a>
+                        <a class="collapse-item" href="Gestion_Ventas.php">Gestion de Ventas</a>
+                    </div>
+                </div>
             </li>
 
 
@@ -142,65 +151,6 @@
             <!-- Main Content -->
             <div id="content">
 
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-
-                    <!-- Topbar Search -->
-                    <form
-                        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                                aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-
-                    <!-- Topbar Navbar -->
-                    <ul class="navbar-nav ml-auto">
-
-                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
-                                            placeholder="Search for..." aria-label="Search"
-                                            aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
-
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="../../public/login.php" data-toggle="modal" data-target="#logoutModal""
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-800 big">Cerrar Sesion</span>
-                            </a>
-                        </li>
-
-                    </ul>
-
-                </nav>
-                <!-- End of Topbar -->
+                
+    <!-- End of Page Wrapper -->
                  
--- ProyectoDesarrollo/app/views/productos.php
+++ ProyectoDesarrollo/app/views/productos.php
@@ -6,37 +6,41 @@ require_once '../controllers/ProductosController.php';
 $ProductosController = new ProductController();
 
 $products = $ProductosController->index();
-
-
 ?>
 
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-2 text-gray-800">Productos</h1>
+    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+        <h1 class="h3 mb-3 text-gray-800">Productos</h1>
+    </nav>
 
     <!-- Content Row -->
     <div class="row">
 
-        <div class="col-xl-8 col-lg-7">
+        <div class="col-xl-12 col-lg-7">
 
             <div class="card shadow mb-4">
-                <div class="card-header py-3">
+                <div class="card-header py-3 d-sm-flex align-items-center justify-content-between mb-4">
                     <h6 class="m-0 font-weight-bold text-primary">Listado de Productos</h6>
+                    <Button class=" btn btn-sm btn-primary shadow-sm">
+                        <i class="bx bxs-file-plus"></i>Agregar Productos
+                    </Button>
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
-                        <table class="table table-bordered" id="productosTable" width="100%" cellspacing="0">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                             <thead>
                                 <tr>    
-                                    <th>Código</th>
+                                    <th>ID</th>
                                     <th>Nombre de Producto</th>
                                     <th>Descripcion</th>
                                     <th>Tipo de Producto</th>
                                     <th>Laboratorio</th>
                                     <th>Costo</th>
                                     <th>Precio de venta</th>
+                                    <th>Acciones</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -45,14 +49,18 @@ $products = $ProductosController->index();
                                         <td><?php echo $product['CODIGO_PRODUCTO']; ?></td>
                                         <td><?php echo $product['NOMBRE_PRODUCTO']; ?></td>
                                         <td><?php echo $product['DESCRIPCION']; ?></td>
-                                        <td><?php echo$product['TIPO_PRODUCTO']; ?></td>
+                                        <td><?php echo $product['TIPO_PRODUCTO']; ?></td>
                                         <td><?php echo $product['LABORATORIO']; ?></td>
                                         <td><?php echo $product['COSTO']; ?></td>
                                         <td><?php echo $product['PRECIO_VENTA']; ?></td>
+                                        <td>
+                                            <button type="button" class="btn btn-sm btn-circle btn-primary bx bxs-file-plus"></button>
+                                            <button type="button" class="btn btn-sm btn-circle btn-warning bx bx-edit"></button>
+                                            <button type="button" class="btn btn-sm btn-circle btn-danger bx bx-trash"></button>
+                                        </td>
                                     </tr>
                                 <?php endforeach; ?>
-                        </tbody>
-
+                            </tbody>
                         </table>
                     </div>
                 </div>
@@ -60,23 +68,10 @@ $products = $ProductosController->index();
 
         </div>
 
-        <!-- Donut Chart -->
-        <div class="col-xl-4 col-lg-5">
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Detalles del Producto</h6>
-                </div>
-                <div class="card-body" id="productoDetalles">
-                    <!-- Aquí se llenará la información del producto -->
-                </div>
-            </div>
-        </div>
     </div>
 
 </div>
 <!-- /.container-fluid -->
-
-</div>
 
 <?php
 require_once "parte_inferior.php";
