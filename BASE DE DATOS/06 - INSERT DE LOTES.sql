3d4ff67948a037db5a37ebfdd4bff6d773e4f1ed
NathaIsmatul <esdrasismatul@hotmail.com>
2024-10-25 03:49:58 -0600

    Create 06 - INSERT DE LOTES.sql
    Inserts de pruebas para los lotes y stock entre lotes y productos

---
+++ BASE DE DATOS/06 - INSERT DE LOTES.sql
@@ -0,0 +1,8 @@
+insert into LOTES_INVENTARIO(UNID_ENTRANTES,VENCIMIENTO,ID_PRODUCTO,ID_PROVEEDOR,COMENTARIO)
+values(10,'2024-12-07',1,1,'producto de nuevo ingreso'),
+	  (8,'2024-10-07',1,1,'producto de nuevo ingreso'),
+	  (3,'2020-11-20',2,2,'producto de nuevo ingreso'),
+	  (12,'2024-11-20',2,2,'producto de nuevo ingreso'),
+	  (7,'2024-10-25',4,4,'producto de nuevo ingreso'),
+      (7,'2024-11-25',4,4,'producto de nuevo ingreso');
+      
